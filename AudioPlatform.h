e327fc2fabc7f6ff75ed793dc6a5f654373c0ba0
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2025-07-06 23:21:24 -0500

    Only defined constants if they are not already defined

--- AudioPlatform.h
+++ AudioPlatform.h
@@ -5,7 +5,12 @@
 #include <AudioStream.h>
 #include <arm_math.h>
 
+#ifndef DELAY_QUEUE_SIZE
 #define DELAY_QUEUE_SIZE  (176512 / AUDIO_BLOCK_SAMPLES)
+#endif
+
+#ifndef MAX_BUFFERS
 #define MAX_BUFFERS 209
+#endif
 
 #endif
